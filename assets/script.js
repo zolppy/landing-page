8d3f48e11144b2ae65850d25fb9529833ba8a474
Gabriel Cavalcante <gabriel.lcifba@gmail.com>
2024-08-07 17:24:11 -0300

    fix: corrijido erros no js

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,13 @@
-const getScreenWidth = () => {
+const handleResize = () => {
   const screenWidth = window.innerWidth;
-
-  return screenWidth;
-};
-
-document.addEventListener("DOMContentLoaded", () => {
-  const screenWidth = getScreenWidth();
+  const block = document.querySelector(".block");
 
   if (screenWidth < 900) {
-    const body = document.querySelector("body");
-    const main = document.createElement("main");
-    const section = document.createElement("section");
-    const p = document.createElement("p");
-
-    p.innerHTML = "Disponível <br /> para <br /> desktop";
-    p.style.fontSize = "24px";
-    p.style.fontWeight = "bold";
-    p.style.textAlign = "center";
-    section.appendChild(p);
-    section.style.display = "grid";
-    section.style.minHeight = "100dvh";
-    section.style.placeItems = "center";
-    main.appendChild(section);
-    body.innerHTML = "";
-    body.appendChild(main);
+    block.style.display = "grid";
+  } else {
+    block.style.display = "none";
   }
-});
+};
+
+document.addEventListener("DOMContentLoaded", handleResize);
+window.addEventListener("resize", handleResize);
