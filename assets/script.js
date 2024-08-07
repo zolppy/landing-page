e232a2501608b75813c8fee87c36086bf5e8f4ea
Gabriel Cavalcante <gabriel.lcifba@gmail.com>
2024-08-07 16:43:34 -0300

    fix: correção de textos

--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
   if (screenWidth < 900) {
     body.textContent = "";
     body.innerHTML =
-      "<main><section><h1>Use uma tela maior</h1></section></main>";
+      "<main><section><h1>Disponível para desktop</h1></section></main>";
     body.style.display = "grid";
     body.style.minHeight = "100dvh";
     body.style.placeItems = "center";
