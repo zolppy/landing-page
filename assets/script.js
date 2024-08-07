fba94684654b107d0a5df3257c7c2cf2fb0fffe6
Gabriel Cavalcante <gabriel.lcifba@gmail.com>
2024-08-07 18:07:59 -0300

    fix: ajustes finais

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,17 @@
 const handleResize = () => {
   const screenWidth = window.innerWidth;
+  const main = document.querySelector("main");
   const block = document.querySelector(".block");
+  const footer = document.querySelector("footer");
 
   if (screenWidth < 900) {
     block.style.display = "grid";
+    main.style.display = "none";
+    footer.style.display = "none";
   } else {
     block.style.display = "none";
+    main.style.display = "block";
+    footer.style.display = "flex";
   }
 };
 
